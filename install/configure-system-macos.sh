79d87b278395dfb7b450d8f7d6c97ed650225161
Hasib Rahman <hasibr.me@gmail.com>
2023-07-31 21:54:24 -0400

    chore: Do not kill apps on system configuration

--- install/configure-system-macos.sh
+++ install/configure-system-macos.sh
@@ -929,27 +929,27 @@ defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 # # Kill affected applications                                                  #
 # ###############################################################################
 
-for app in "Activity Monitor" \
-	"Address Book" \
-	"Calendar" \
-	"cfprefsd" \
-	"Contacts" \
-	"Dock" \
-	"Finder" \
-	# "Google Chrome Canary" \
-	# "Google Chrome" \
-	"Mail" \
-	"Messages" \
-	# "Opera" \
-	"Photos" \
-	"Safari" \
-	"SizeUp" \
-	"Spectacle" \
-	"SystemUIServer" \
-	# "Terminal" \
-	# "Transmission" \
-	# "Tweetbot" \
-	# "Twitter" \
-	"iCal"; do
-	killall "${app}" &> /dev/null
-done
+#for app in "Activity Monitor" \
+#	"Address Book" \
+#	"Calendar" \
+#	"cfprefsd" \
+#	"Contacts" \
+#	"Dock" \
+#	"Finder" \
+#	# "Google Chrome Canary" \
+#	# "Google Chrome" \
+#	"Mail" \
+#	"Messages" \
+#	# "Opera" \
+#	"Photos" \
+#	"Safari" \
+#	"SizeUp" \
+#	"Spectacle" \
+#	"SystemUIServer" \
+#	# "Terminal" \
+#	# "Transmission" \
+#	# "Tweetbot" \
+#	# "Twitter" \
+#	"iCal"; do
+#	killall "${app}" &> /dev/null
+#done
